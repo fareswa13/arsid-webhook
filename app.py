b43baeb39dd504f2defd225a85f6c2b138071d42
fareswa13 <fares.srttt@gmail.com>
2025-05-01 21:51:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def salla_webhook():
 @app.route('/')
 def home():
     return "Arsid Webhook is live ✅"
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
